13738a54b65a9aab4fe3e7855aa168d20a5f5453
Shahar rozolio <shahartheking22@gmail.com>
2022-07-03 16:32:10 +0300

    move cell abstracts

---
+++ GetScwifty/BL/Moves/MoveCell.js
@@ -0,0 +1,9 @@
+class MoveCell {
+    constructor (possibleMoves, GameGrid) {
+        this.possibleMoves = possibleMoves
+        this.GameGrid = GameGrid
+    }
+    Swap(cell) {
+        // bool returns true if succeeds else false
+    }
+}
