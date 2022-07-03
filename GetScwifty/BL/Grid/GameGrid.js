74942f103a603c9e7516e676106078714f271db3
Shahar rozolio <shahartheking22@gmail.com>
2022-07-03 17:40:29 +0300

    Random grid

--- GetScwifty/BL/Grid/GameGrid.js
+++ GetScwifty/BL/Grid/GameGrid.js
@@ -2,7 +2,7 @@ class GameGrid extends GridBase {
 
     constructor (length, cellsFactory) {
         super(length)
-        this.emptyCell = null,
+        this.emptyCell = null
         this.cellsFactory = cellsFactory
     }
 
@@ -13,16 +13,26 @@ class GameGrid extends GridBase {
         }
     }
     MixGrid() {
-
+        this.MixCells()
+        this.EmptyCellPositioning()
     }
 
-    EmptyCell() {
-        x, y = Math.floor(Math.random() * this.length) + 1, Math.floor(Math.random() * this.length) + 1
-        
-
+    EmptyCellPositioning() {
+        let x = Math.floor(Math.random() * this.length) + 1
+        let y = Math.floor(Math.random() * this.length) + 1
+        this.emptyCell = new cellBase(this.length - 1, this.length - 1, this.emptyCellValue)
+        this.cells[this.length - 1].push(this.emptyCell)
+        this.SwapCells(this.cells[x][y], this.emptyCell)
+    }
+    
+    SwapCells(cell1, cell2) {
+        this.cells[cell1.x][cell1.y] = cell2.value
+        this.cells[cell2.x][cell2.y] = cell1.value
+        [cell1.x, cell2.x] = [cell2.x, cell1.x]
+        [cell1.y, cell2.y] = [cell2.y, cell1.y]
     }
 
     SwapWithEmptyCell(cell) {
-
+        this.SwapCells(cell, this.emptyCell)
     }
 }
--- GetScwifty/Common/GridCommon/GridBase.js
+++ GetScwifty/Common/GridCommon/GridBase.js
@@ -1,7 +1,7 @@
-const NOT_INIT_CELL = 'EMPTY'
+const EMPTY_CELL = 'EMPTY'
 class GridBase {
     constructor(length) {
-        this.not_init_cell = NOT_INIT_CELL
+        this.emptyCellValue = EMPTY_CELL
         this.length = length
         this.numOfCells = length * length
         this.cells = []
