bb9fbb61f034da17b9237f01cdc9a3fc9922092f
Shahar rozolio <shahartheking22@gmail.com>
2022-07-03 16:50:58 +0300

    adding cells factory

--- GetScwifty/Common/GridCommon/CreateRandomCellFactory.js
+++ GetScwifty/Common/GridCommon/CreateRandomCellFactory.js
@@ -1,5 +1,13 @@
 class CreateRandomCellFactory {
-    CreateRandomCell (cellGrid) {
-        
+    
+    CreateRandomCell (numberOfCells) {
+        let cells = []
+        while(cells.length < numberOfCells) {
+            let cellValue = Math.floor(Math.random() * 100) + 1;
+            if(cells.indexOf(cellValue) === -1) {
+                cells.push(cellValue)
+            } 
+        }
+        return cells
     }
 }
