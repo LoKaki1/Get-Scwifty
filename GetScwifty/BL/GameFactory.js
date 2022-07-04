07a70a5d7c4c65d1150d46da189326e7bbc6f3be
Shahar rozolio <shahartheking22@gmail.com>
2022-07-04 11:35:10 +0300

    create gameFactory class

--- GetScwifty/BL/GameFactory.js
+++ GetScwifty/BL/GameFactory.js
@@ -1,3 +1,12 @@
-export class GameFactory() {
-    
+import { GameLogic } from "./GameLogic.js"
+
+export class GameFactory {
+    constructor (cellsFactory) {
+        this.cellsFactory = cellsFactory
+    }
+
+    CreateGame() {
+        let gameLogic = new GameLogic(this.cellsFactory)
+        return gameLogic
+    }
 }
