53c65a615e01b10464adc36d6350247cb84c7de7
Shahar rozolio <shahartheking22@gmail.com>
2022-07-04 16:36:28 +0300

    creating  mover view

--- GetScwifty/ViewController/Moves/PossibleMovesViewController.js
+++ GetScwifty/ViewController/Moves/PossibleMovesViewController.js
@@ -1,17 +1,17 @@
 export class PossibleMovesViewController {
 
-    constructor (movesView) {
-        this.movesView = movesView
+    constructor (possibleMovesView) {
+        this.possibleMovesView = possibleMovesView
     }
 
     ShowPossibleMoves(game) {
         let possibleMoves = game.GetMoves()
-        this.movesView.ViewMoves(possibleMoves, game.gameGrid.length) 
+        this.possibleMovesView.ViewMoves(possibleMoves, game.gameGrid.length) 
     }
 
     PossibleMovesListener(game) {
         // Think about giving the grid instead
-        this.movesView.AddListenerToCell(game.gameGrid.emptyCell, game.gameGrid.length, () => {
+        this.possibleMovesView.AddListenerToCell(game.gameGrid.emptyCell, game.gameGrid.length, () => {
             this.ShowPossibleMoves(game)
         })
     }
